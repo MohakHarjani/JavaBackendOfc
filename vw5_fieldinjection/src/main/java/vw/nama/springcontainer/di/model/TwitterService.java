5b855f64a061199917bcc360ed465e81bff3258a
MohakHarjani <mh2001harjani@gmail.com>
2024-02-07 22:23:18 +0530

    final

---
+++ vw5_fieldinjection/src/main/java/vw/nama/springcontainer/di/model/TwitterService.java
@@ -0,0 +1,15 @@
+package vw.nama.springcontainer.di.model;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("TwitterService")
+public class TwitterService implements MessageService{
+
+    @Override
+    public void sendMessage(String message){
+        System.out.println(message);
+    }
+}
